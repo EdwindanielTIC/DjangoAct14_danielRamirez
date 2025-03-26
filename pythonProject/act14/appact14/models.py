a515d3a0eca628669b6466c1c9412faebf6de77e
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-03-26 18:58:08 +0100

    models creat, falta el docker

--- pythonProject/act14/appact14/models.py
+++ pythonProject/act14/appact14/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)  # ID autogenerado
+    username = models.CharField(max_length=50, unique=True)  # Campo único
+    email = models.EmailField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
